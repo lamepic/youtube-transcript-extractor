9a9fcf7c26148b8b73dcf8bbe797320a62abcc3b
Richard Arthur <richardarthur228@gmail.com>
2024-02-07 23:38:35 -0600

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
 import "./style.css";
+
+const app = document.getElementById("app");
+const button = document.createElement("button");
+button.innerText = "Click to load transcript";
+const loader = document.createElement("p");
+loader.innerHTML = "Loading...";
+
+const url = new URL(window.location.href);
+const video_id = url.searchParams.get("v");
+console.log(window.location.href);
+if (video_id) {
+  app?.appendChild(button);
+
+  button.addEventListener("click", async function () {
+    app?.removeChild(button);
+    app?.appendChild(loader);
+    const { data } = await fetchTranscript(video_id);
+    const text = document.createTextNode(data);
+    app?.removeChild(loader);
+    app?.appendChild(text);
+  });
+} else {
+  const p = document.createElement("p");
+  p.innerHTML = "No Transcript found on this page.";
+  app?.appendChild(p);
+}
+
+async function fetchTranscript(video_id: string) {
+  const res = await fetch(`http://localhost:3000/api?video_id=${video_id}`);
+  const data = await res.json();
+  return data;
+}
