eabd43cd8594ba882da3bffc3a7d45d2ac7d9fa0
Richard Arthur <richardarthur228@gmail.com>
2024-02-09 14:30:26 -0600

    Add main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Youtube Trascript Extractor</h1>
+    <main className="w-7/12 mx-auto flex flex-col items-center justify-center gap-10">
+      <h1 className="mt-20 text-center text-4xl font-semibold">
+        Youtube Trascript Extractor
+      </h1>
+
+      <form className="w-10/12">
+        <div className="border flex items-center overflow-hidden rounded-md">
+          <label htmlFor="url" className="px-5 py-3 bg-slate-400/40">
+            URL
+          </label>
+          <input
+            name="url"
+            type="text"
+            className="w-full outline-1 outline-slate-400 py-3"
+          />
+        </div>
+      </form>
+
+      <div className="mt-5 text-justify p-2 max-h-[500px] overflow-scroll leading-7"></div>
     </main>
   );
 }
