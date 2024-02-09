bbfc757f9307dfced2c78602c9a0daeb1f484401
Richard Arthur <richardarthur228@gmail.com>
2024-02-09 16:24:42 -0600

    Add Loader

---
+++ components/Loader.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+function Loader() {
+  return (
+    <div className="h-[80%] grid place-items-center">
+      <Image
+        src="/three-dots.svg"
+        className="h-20 w-20"
+        alt="loader"
+        height={100}
+        width={100}
+      />
+    </div>
+  );
+}
+
+export default Loader;
